efcd7a852c4847b2fadc7947a20921d599d9436c
Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>
2015-04-20 17:56:00 +0300

    ext: Do not remove disks if --keep-disks and DT_EXT
    This will remove the disk from the config.data but the actual
    data will not be removed (and thus can be used later).
    
    Signed-off-by: Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>

--- lib/cmdlib/instance_utils.py
+++ lib/cmdlib/instance_utils.py
@@ -346,6 +346,9 @@ def RemoveDisks(lu, instance, disks=None,
     else:
       edata = device.ComputeNodeTree(instance.primary_node)
     for node_uuid, disk in edata:
+      if getattr(lu.op, "keep_disks", None):
+        if lu.op.keep_disks and disk.dev_type == constants.DT_EXT:
+          continue
       result = lu.rpc.call_blockdev_remove(node_uuid, (disk, instance))
       if result.fail_msg:
         lu.LogWarning("Could not remove disk %s on node %s,"
